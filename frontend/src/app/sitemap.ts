dd7706d46ba2e5ade4915acb4b76309727f38086
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-08-29 10:56:42 -0500

    fix: add terminos-de-servicio to sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -32,5 +32,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.3,
     },
+    {
+      url: "https://enderhost.net.pe/terminos-de-servicio",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
   ];
 }
